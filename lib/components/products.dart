b7c9b013cfa86843fb286f741ac55780665a6c5f
ZIN MYO <overmidnight12@gmail.com>
2020-04-09 11:39:57 -0700

    my changes

--- lib/components/products.dart
+++ lib/components/products.dart
@@ -6,8 +6,41 @@ class Products extends StatefulWidget {
 }
 
 class _ProductsState extends State<Products> {
+  var product_list = [
+    {
+      "name":"Uniqlo T-SHIRT",
+      "picture": "images/products/un (1).jpg",
+      "old_price": 18,
+      "price": 14,
+    },
+    {
+      "name":"Uniqlo T-SHIRT",
+      "picture": "images/products/un (1).png",
+      "old_price": 18,
+      "price": 14,
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
+
+class Single_prod extends StatelessWidget {
+  final prod_name;
+  final prod_picture;
+  final prod_old_price;
+  final prod_price;
+
+  Single_prod({
+    this.prod_name,
+    this.prod_picture,
+    this.prod_old_price,
+    this.prod_price,
+});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
