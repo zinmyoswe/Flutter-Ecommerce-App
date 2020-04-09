af174610de054e1bcceae50714764ecf10e83604
ZIN MYO <overmidnight12@gmail.com>
2020-04-09 06:00:20 -0700

    my changes

--- lib/components/horizontal_listview.dart
+++ lib/components/horizontal_listview.dart
@@ -8,7 +8,30 @@ class HorizontalList extends StatelessWidget {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: <Widget>[
-
+          Category(
+            image_location: 'images/cats/ca1.png',
+            image_caption: 'shoes',
+          ),
+          Category(
+            image_location: 'images/cats/ca2.png',
+            image_caption: 'shoes',
+          ),
+          Category(
+            image_location: 'images/cats/ca3.png',
+            image_caption: 'shoes',
+          ),
+          Category(
+            image_location: 'images/cats/ca4.png',
+            image_caption: 'shoes',
+          ),
+          Category(
+            image_location: 'images/cats/ca5.png',
+            image_caption: 'shoes',
+          ),
+          Category(
+            image_location: 'images/cats/ca6.png',
+            image_caption: 'shoes',
+          ),
         ],
       ),
     );
@@ -22,15 +45,24 @@ class Category extends StatelessWidget {
   Category({
     this.image_location,
     this.image_caption
-})
+});
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.all(2.0),
-    child: InkWell(onTap: (){},
-    child: ListTile(
-      title: Image.asset(image_location),
-      subtitle: Text(image_caption),
-    ),
+    return Padding(padding: const EdgeInsets.all(0.0),
+    child: InkWell(
+      onTap: (){},
+      child: Container(
+        width: 150.0,
+
+      child: ListTile(
+        title: Image.asset(
+            image_location,
+            width: 150.0,
+            height: 150.0,
+        ),
+        subtitle: Text(image_caption),
+        ),
+      ),
     ),
     );
   }
